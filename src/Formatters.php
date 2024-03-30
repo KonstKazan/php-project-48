ad613638893194b2ac168d8b1c1ca9f89bb10923
Konstantin <konst.kazan@gmail.com>
2024-03-30 14:02:45 +0300

    Add changes

--- src/Formatters.php
+++ src/Formatters.php
@@ -11,13 +11,10 @@ function formatThree(array $three, string $format): string|false
     switch ($format) {
         case 'stylish':
             return formatStylish($three);
-            break;
         case 'plain':
             return formatPlain(($three));
-            break;
         case 'json':
             return formatJson($three);
-            break;
         default:
             throw new \Exception("$format is unknow format!");
     }
--- src/Parsers.php
+++ src/Parsers.php
@@ -8,23 +8,36 @@ function getRealPath(string $pathToFile): mixed
 {
     $addedPart = $pathToFile[0] === '/' ? '' : __DIR__ . "/../";
     $fullPath = $addedPart . $pathToFile;
-
     $realPath = realpath($fullPath);
+    if ($realPath === false) {
+        throw new \Exception("File not exists");
+    }
     return $realPath;
+}
+
+function getExtension(string $pathToFile): string
+{
+    return pathinfo($pathToFile, PATHINFO_EXTENSION);
+}
+
+function getFile(string $path): string
+{
+    $file = file_get_contents($path);
+    if ($file === false) {
+        throw new \Exception("Path to file is invalid!");
+    }
+    return $file;
 }
 
 function parse(string $pathToFile,): array
 {
     $path = getRealPath($pathToFile);
-    $file = file_get_contents($path);
-    $extension = pathinfo($pathToFile, PATHINFO_EXTENSION);
-    if ($file === false) {
-        throw new \Exception("Path to file is invalid!");
-    }
+    $file = getFile($path);
+    $extension = getExtension($pathToFile);
+
     switch ($extension) {
         case 'json':
             return json_decode($file, true);
-            break;
         case 'yaml':
         case 'yml':
             return Yaml::parse($file);
